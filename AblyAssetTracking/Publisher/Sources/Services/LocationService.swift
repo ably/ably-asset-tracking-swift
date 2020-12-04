fbb9db67daf59b14e661dbb5736a2d90d6a87761
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-04 17:16:21 +0100

    [ATS-38] Remove comment headers from source files

--- AblyAssetTracking/Publisher/Sources/Services/LocationService.swift
+++ AblyAssetTracking/Publisher/Sources/Services/LocationService.swift
@@ -1,11 +1,3 @@
-//
-//  MapBoxService.swift
-//  Publisher
-//
-//  Created by Michal Miedlarz on 02/12/2020.
-//  Copyright © 2020 Ably. All rights reserved.
-//
-
 import CoreLocation
 import MapboxCoreNavigation
 
