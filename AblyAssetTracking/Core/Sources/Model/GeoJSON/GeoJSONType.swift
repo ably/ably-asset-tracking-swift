18a72cd6c96a0ef087fdc96de572c08bf3681669
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-07 11:16:10 +0100

    [ATS-38] Remove headers

--- AblyAssetTracking/Core/Sources/Model/GeoJSON/GeoJSONType.swift
+++ AblyAssetTracking/Core/Sources/Model/GeoJSON/GeoJSONType.swift
@@ -1,11 +1,3 @@
-//
-//  GeoJSONType.swift
-//  AblyAssetTracking
-//
-//  Created by Michal Miedlarz on 02/12/2020.
-//  Copyright © 2020 Ably. All rights reserved.
-//
-
 import Foundation
 
 enum GeoJSONType: String, Codable {
