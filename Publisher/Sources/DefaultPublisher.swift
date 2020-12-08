85343668be454d87450d2c13c4a442d609132dcf
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-08 13:20:49 +0100

    [ATS-66] Implement track method of DefaultPublisher

--- Publisher/Sources/DefaultPublisher.swift
+++ Publisher/Sources/DefaultPublisher.swift
@@ -5,11 +5,11 @@ public class DefaultPublisher: AssetTrackingPublisher {
     private let configuration: AssetTrackingPublisherConfiguration
     private let locationService: LocationService
     private let ablyService: AblyPublisherService
-    
+
     public weak var delegate: AssetTrackingPublisherDelegate?
     private(set) public var activeTrackable: Trackable?
     public var transportationMode: TransportationMode
-    
+
     /**
      Default constructor. Initializes Publisher with given `AssetTrackingPublisherConfiguration`.
      Publisher starts listening (and notifying delegate) after initialization.
@@ -21,38 +21,38 @@ public class DefaultPublisher: AssetTrackingPublisher {
         self.locationService = LocationService()
         self.ablyService = AblyPublisherService(apiKey: configuration.apiKey,
                                                 clientId: configuration.clientId)
-                
+
         // TODO: Set proper values from configuration
         self.activeTrackable = nil
         self.transportationMode = TransportationMode()
         self.ablyService.delegate = self
         self.locationService.delegate = self
     }
-    
+
     public func track(trackable: Trackable) {
         activeTrackable = trackable
         ablyService.track(trackable: trackable) { [weak self] error in
             guard let self = self else { return }
-            
+
             if let error = error {
                 self.delegate?.assetTrackingPublisher(sender: self, didFailWithError: error)
             }
             self.locationService.startUpdatingLocation()
         }
     }
-    
+
     public func add(trackable: Trackable) {
         // TODO: Implement method
         failWithNotYetImplemented()
     }
-    
+
     public func remove(trackable: Trackable) -> Bool {
         // TODO: Implement method
         failWithNotYetImplemented()
-        
+
         return false;
     }
-    
+
     public func stop() {
         // TODO: Implement method
         failWithNotYetImplemented()
@@ -63,7 +63,7 @@ extension DefaultPublisher: LocationServiceDelegate {
     func locationService(sender: LocationService, didFailWithError error: Error) {
         delegate?.assetTrackingPublisher(sender: self, didFailWithError: error)
     }
-    
+
     func locationService(sender: LocationService, didUpdateRawLocation location: CLLocation) {
         delegate?.assetTrackingPublisher(sender: self, didUpdateRawLocation: location)
         ablyService.sendRawAssetLocation(location: location) { [weak self] error in
@@ -73,7 +73,7 @@ extension DefaultPublisher: LocationServiceDelegate {
             }
         }
     }
-    
+
     func locationService(sender: LocationService, didUpdateEnhancedLocation location: CLLocation) {
         delegate?.assetTrackingPublisher(sender: self, didUpdateEnhancedLocation: location)
         ablyService.sendEnhancedAssetLocation(location: location) { [weak self] error in
