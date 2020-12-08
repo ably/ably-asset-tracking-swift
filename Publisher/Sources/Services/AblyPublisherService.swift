13aec1b33ee92eb784eea5c35bacfbac88ffb7b0
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-08 19:06:59 +0100

    [ATS-66] Set channel correctly

--- Publisher/Sources/Services/AblyPublisherService.swift
+++ Publisher/Sources/Services/AblyPublisherService.swift
@@ -10,20 +10,20 @@ class AblyPublisherService {
     private let clientId: String
     private let presenceData: PresenceData
     private var channel: ARTRealtimeChannel?
-    
+
     weak var delegate: AblyPublisherServiceDelegate?
     var connectionState: AblyConnectionStatus {
         return client.connection.state.toAblyConnectionStatus()
     }
-    
+
     init(apiKey: String, clientId: String) {
         self.clientId = clientId
         self.client = ARTRealtime(key: apiKey)
         self.presenceData = PresenceData(type: .publisher)
-        
+
         setup()
     }
-    
+
     private func setup() {
         // TODO: Log suitable message when Logger become available:
         // https://github.com/ably/ably-asset-tracking-cocoa/issues/8
@@ -37,14 +37,14 @@ class AblyPublisherService {
             )
         }
     }
-    
+
     // MARK: Main interface
     func track(trackable: Trackable, completion: ((Error?) -> Void)?) {
-        precondition(channel == nil, "In current SDK version, service can track only one asset per instance")        
-        
+        precondition(channel == nil, "In current SDK version, service can track only one asset per instance")
+
         // Force cast allowed here, as presence data is our internal class which should never fail to encode
         let data = presenceData.toEncodedJSONString()!
-        
+
         channel = client.channels.get(trackable.id)
         channel?.presence.enterClient(clientId, data: data) { error in
             // TODO: Log suitable message when Logger become available:
@@ -52,33 +52,33 @@ class AblyPublisherService {
             completion?(error)
         }
     }
-    
+
     func sendRawAssetLocation(location: CLLocation, completion: ((Error?) -> Void)?) {
         sendAssetLocation(location: location, withName: .raw, completion: completion)
     }
-    
+
     func sendEnhancedAssetLocation(location: CLLocation, completion: ((Error?) -> Void)?) {
         sendAssetLocation(location: location, withName: .enhanced, completion: completion)
     }
-    
+
     private func sendAssetLocation(location: CLLocation, withName name: EventName, completion: ((Error?) -> Void)?) {
         guard let channel = channel else {
             completion?(AblyError.publisherError("Attempt to send location while not connected to any channel"))
             return
         }
-                    
+
         let geoJSON = GeoJSONMessage(location: location)
         guard let data = [geoJSON].toEncodedJSONString() else {
             completion?(AblyError.inconsistentData("Cannot encode location data to JSON"))
             return
         }
-        
+
         let message = ARTMessage(name: name.rawValue, data: data)
         channel.publish([message]) { errorInfo in
             completion?(errorInfo)
         }
     }
-    
+
     func stop() {
         // TODO: Should we clear channel here? Can AblyService be restarted?
         client.close()
