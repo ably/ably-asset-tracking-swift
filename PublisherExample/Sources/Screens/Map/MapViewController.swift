2a6ca7e7f54f3fb1ae7cabee8717473a9ee8cef7
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-08 15:19:54 +0100

    [ATS-69] Basic MapViewController

--- PublisherExample/Sources/Screens/Map/MapViewController.swift
+++ PublisherExample/Sources/Screens/Map/MapViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
+import MapKit
+import Publisher
 
 class MapViewController: UIViewController {
+    @IBOutlet private weak var mapView: MKMapView!
     private let trackingId: String
+    private var publisher: AssetTrackingPublisher?
+    
+    private var rawLocation: CLLocation?
+    private var enhancedLocation: CLLocation?
     
     // MARK: Initialization
     init(trackingId: String) {
@@ -17,5 +24,75 @@ class MapViewController: UIViewController {
     // MARK: View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPublisher()
+        
+    }
+    
+    // MARK: View setup
+    private func setupPublisher() {
+        let configuration = AssetTrackingPublisherConfiguration(apiKey: "", clientId: "")
+        let trackable = DefaultTrackable(id: trackingId,
+                                         metadata: "Metadata",
+                                         latitude: 0,
+                                         longitude: 0)
+        
+        publisher = DefaultPublisher(configuration: configuration)
+        publisher?.track(trackable: trackable)
+    }
+    
+    // MARK: Utils
+    private func refreshAnnotations() {
+        mapView.annotations.forEach { mapView.removeAnnotation($0) }
+        
+        if let location = rawLocation {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            annotation.title = "Raw"
+            mapView.addAnnotation(annotation)
+        }
+        if let location = enhancedLocation {
+            let annotation = MKPointAnnotation()
+            annotation.title = "Enhanced"
+            annotation.coordinate = location.coordinate
+            mapView.addAnnotation(annotation)
+        }
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { return nil }
+
+        let identifier = "Annotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView!.canShowCallout = false
+        } else {
+            annotationView!.annotation = annotation
+        }
+
+        return annotationView
+    }
+}
+
+extension MapViewController: AssetTrackingPublisherDelegate {
+    func assetTrackingPublisher(sender: AssetTrackingPublisher, didFailWithError error: Error) {
+        
+    }
+    
+    func assetTrackingPublisher(sender: AssetTrackingPublisher, didUpdateRawLocation location: CLLocation) {
+        rawLocation = location
+        refreshAnnotations()
+    }
+    
+    func assetTrackingPublisher(sender: AssetTrackingPublisher, didUpdateEnhancedLocation location: CLLocation) {
+        enhancedLocation = location
+        refreshAnnotations()
+    }
+    
+    func assetTrackingPublisher(sender: AssetTrackingPublisher, didChangeConnectionStatus status: AblyConnectionStatus) {
+        
     }
 }
