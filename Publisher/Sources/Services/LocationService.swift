6779811eef1e9d529590327d80186670ae42ca41
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-10 11:37:00 +0100

    [ATS-94] Add logs to location service

--- Publisher/Sources/Services/LocationService.swift
+++ Publisher/Sources/Services/LocationService.swift
@@ -1,5 +1,6 @@
 import CoreLocation
 import MapboxCoreNavigation
+import os.log
 
 protocol LocationServiceDelegate: class {
     func locationService(sender: LocationService, didFailWithError error: Error)
@@ -19,10 +20,9 @@ class LocationService {
     
     func startUpdatingLocation() {
         locationDataSource.startUpdatingLocation { [weak self] (error) in
-            // TODO: Log suitable message when Logger become available:
-            // https://github.com/ably/ably-asset-tracking-cocoa/issues/8
             if let error = error,
                let strongSelf = self {
+                os_log("Error while updating location: %{public}@", log: .location, type: .error, error as CVarArg)
                 strongSelf.delegate?.locationService(sender: strongSelf, didFailWithError: error)
             }
         }
@@ -43,21 +43,23 @@ class LocationService {
 
 extension LocationService: PassiveLocationDataSourceDelegate {
     func passiveLocationDataSource(_ dataSource: PassiveLocationDataSource, didUpdateLocation location: CLLocation, rawLocation: CLLocation) {
+        os_log(".passiveLocationDataSource.didUpdateLocation. location: %{private}@, rawLocation: %{private}@",
+               log: .location, type: .info, location)
+        
         delegate?.locationService(sender: self, didUpdateRawLocation: rawLocation)
         delegate?.locationService(sender: self, didUpdateEnhancedLocation: location)
     }
     
     func passiveLocationDataSource(_ dataSource: PassiveLocationDataSource, didFailWithError error: Error) {
+        os_log("passiveLocationDataSource.didFailWithError. error:%{public}@", log: .location, type: .error, error as CVarArg)
         delegate?.locationService(sender: self, didFailWithError: error)
     }
     
     func passiveLocationDataSource(_ dataSource: PassiveLocationDataSource, didUpdateHeading newHeading: CLHeading) {
-        // TODO: Log suitable message when Logger become available:
-        // https://github.com/ably/ably-asset-tracking-cocoa/issues/8
+        os_log("passiveLocationDataSource.didUpdateHeading. heading:%{private}@", log: .location, type: .debug, newHeading)        
     }
     
     func passiveLocationDataSourceDidChangeAuthorization(_ dataSource: PassiveLocationDataSource) {
-        // TODO: Log suitable message when Logger become available:
-        // https://github.com/ably/ably-asset-tracking-cocoa/issues/8
+        os_log("passiveLocationDataSource.didChangeAuthorization", log: .location, type: .debug)
     }
 }
