8bc59d8ba24aa050f1cacb897fff9053c4a00c37
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-23 12:44:15 +0100

    [ATS-136] Update QuickHelp docs

--- Publisher/Sources/Resolution Policy/ResolutionPolicyHooks.swift
+++ Publisher/Sources/Resolution Policy/ResolutionPolicyHooks.swift
@@ -1,4 +1,9 @@
-protocol ResolutionPolicyHooks {
+ /**
+  Defines the methods which can be called by a resolution policy when it is created.
+  Methods on this interface may only be called from within implementations of
+  `createResolutionPolicy` `Factory.createResolutionPolicy`.
+  */
+ protocol ResolutionPolicyHooks {
     /**
      Register a handler for the addition, removal and activation of `Trackable` objects for the `Publisher`
      instance whose `creation` `Publisher.Builder.start` caused
@@ -15,7 +20,6 @@ protocol ResolutionPolicyHooks {
      Register a handler for the addition and removal of remote `Subscriber`s to the `Publisher` instance whose
      `creation` `Publisher.Builder.start` caused `createResolutionPolicy` `Factory.createResolutionPolicy` to be
      called.
-
      This method should only be called once within the scope of creation of a single publisher's resolution
      policy. Subsequent calls to this method will replace the previous handler.
 
@@ -23,18 +27,18 @@ protocol ResolutionPolicyHooks {
         - listener: The handler, which may be called multiple times during the lifespan of the publisher.
      */
     func subscribers(listener: SubscriberSetListener)
-}
+ }
 
-/**
- A handler of events relating to the addition, removal and activation of `Trackable` objects for a
- `Publisher` instance.
- */
-protocol TrackableSetListener {
+ /**
+  A handler of events relating to the addition, removal and activation of `Trackable` objects for a
+  `Publisher` instance.
+  */
+ protocol TrackableSetListener {
     /**
      A `Trackable` object has been added to the `Publisher`'s set of tracked objects.
-
      If the operation adding `trackable` is also making it the `actively` `Publisher.active` tracked object
      then `onActiveTrackableChanged` will subsequently be called.
+
      - Parameters:
         - trackable: The object which has been added to the tracked set.
      */
@@ -51,15 +55,15 @@ protocol TrackableSetListener {
     func onTrackableRemoved(trackable: Trackable)
 
     /**
-     The `actively``Publisher.active` tracked object has changed.
+     The `actively` `Publisher.active` tracked object has changed.
 
      - Parameters:
         - trackable: The object, from the tracked set, which has been activated - or no value if there is no longer an actively tracked object.
      */
     func onActiveTrackableChanged(trackable: Trackable?)
-}
+ }
 
-protocol SubscriberSetListener {
+ protocol SubscriberSetListener {
     /**
      A `Subscriber` has subscribed to receive updates for one or more `Trackable` objects from the
      `Publisher`'s set of tracked objects.
@@ -72,8 +76,9 @@ protocol SubscriberSetListener {
     /**
      A `Subscriber` has unsubscribed from updates for one or more `Trackable` objects from the `Publisher`'s
      set of tracked objects.
+
      - Parameters:
         - subscriber: The remote entity that unsubscribed.
      */
     func onSubscriberRemoved(subscriber: Subscriber)
-}
+ }
