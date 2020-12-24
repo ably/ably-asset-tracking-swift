a5f4ae7d4abe30e91ad8f2ed02dd02557962bac8
Michal Miedlarz <m.miedlarz@nomtek.com>
2020-12-24 11:05:48 +0100

    [ATS-121] Add PublisherEvents

---
+++ Publisher/Sources/DefaultPublisherEvents.swift
@@ -0,0 +1,75 @@
+import Foundation
+import CoreLocation
+
+protocol PublisherEvent {}
+
+struct StopPublisherEvent: PublisherEvent {}
+struct StartPublisherEvent: PublisherEvent {}
+
+struct SuccessEvent: PublisherEvent {
+    let onSuccess: () -> Void
+}
+
+struct ErrorEvent: PublisherEvent {
+    let exception: Error
+    let onError: (Error) -> Void
+}
+
+struct AddTrackableEvent: PublisherEvent {
+    let trackable: Trackable
+    let onSuccess: () -> Void
+    let onError: (Error) -> Void
+}
+
+struct TrackTrackableEvent: PublisherEvent {
+    let trackable: Trackable
+    let onSuccess: () -> Void
+    let onError: (Error) -> Void
+}
+
+struct RemoveTrackableEvent: PublisherEvent {
+    let trackable: Trackable
+    let onSuccess: (_ wasPresent: Bool) -> Void
+    let onError: (Error) -> Void
+}
+
+struct JoinPresenceSuccessEvent: PublisherEvent {
+    let trackable: Trackable
+//    let channel: Channel
+    let onSuccess: () -> Void
+}
+
+struct TrackableReadyToTrackEvent: PublisherEvent {
+    let trackable: Trackable
+    let onSuccess: () -> Void
+}
+
+struct ClearActiveTrackableEvent: PublisherEvent {
+    let trackable: Trackable
+    let onSuccess: () -> Void
+}
+
+struct RawLocationChangedEvent: PublisherEvent {
+    let location: CLLocation
+    let geoJsonMessage: GeoJSONMessage
+}
+
+struct EnhancedLocationChangedEvent: PublisherEvent {
+    let location: CLLocation
+    let geoJsonMessages: [GeoJSONMessage]
+}
+
+struct SetDestinationEvent: PublisherEvent {
+    let destination: CLLocation
+}
+
+struct RefreshResolutionPolicyEvent: PublisherEvent {}
+
+struct SetDestinationSuccessEvent: PublisherEvent {
+    let routeDurationInMilliseconds: Double
+}
+
+struct PresenceMessageEvent: PublisherEvent {
+    let trackable: Trackable
+//    let presenceMessage: PresenceMessage
+}
