5899654cdfaed010db49c7fe57da2c8daa2e9a9a
Robert Kuraj <r.kuraj@nomtek.com>
2021-02-03 13:38:25 +0100

    [ATS-201] Update publisher tests.

--- Publisher/Tests/Mocks/MockAblyPublisherService.swift
+++ Publisher/Tests/Mocks/MockAblyPublisherService.swift
@@ -3,6 +3,30 @@ import Foundation
 @testable import Publisher
 
 class MockAblyPublisherService: AblyPublisherService {
+    var trackCalled: Bool = false
+    var trackParamTrackable: Trackable?
+    var trackParamResultHandler: ResultHandler<Void>?
+    var trackCompletionHandler: ((ResultHandler<Void>?) -> Void)?
+    func track(trackable: Trackable, completion: ResultHandler<Void>?) {
+        trackCalled = true
+        trackParamTrackable = trackable
+        trackParamResultHandler = completion
+
+        trackCompletionHandler?(completion)
+    }
+    
+    var stopTrackingCalled: Bool = false
+    var stopTrackingParamTrackable: Trackable?
+    var stopTrackingParamResultHandler: ResultHandler<Bool>?
+    var stopTrackingResultCompletionHandler: ((ResultHandler<Bool>?) -> Void)?
+    func stopTracking(trackable: Trackable, completion: ResultHandler<Bool>?) {
+        stopTrackingCalled = true
+        stopTrackingParamTrackable = trackable
+        stopTrackingParamResultHandler = completion
+
+        stopTrackingResultCompletionHandler?(completion)
+    }
+    
     var trackablesGetValue: [Trackable] = []
     var trackables: [Trackable] { return trackablesGetValue }
 
@@ -11,39 +35,11 @@ class MockAblyPublisherService: AblyPublisherService {
         didSet { wasDelegateSet = true }
     }
 
-    var trackCalled: Bool = false
-    var trackParamTrackable: Trackable?
-    var trackParamCompletion: ((Error?) -> Void)?
-    var trackCompletionHandler: ((((Error?) -> Void)?) -> Void)?
-    func track(trackable: Trackable, completion: ((Error?) -> Void)?) {
-        trackCalled = true
-        trackParamTrackable = trackable
-        trackParamCompletion = completion
-
-        trackCompletionHandler?(completion)
-    }
-
-    var stopTrackingCalled: Bool = false
-    var stopTrackingParamTrackable: Trackable?
-    var stopTrackingParamOnSuccess: ((Bool) -> Void)?
-    var stopTrackingParamOnError: ErrorHandler?
-    var stopTrackingOnErrorCompletionHandler: ((ErrorHandler) -> Void)?
-    var stopTrackingOnSuccessCompletionHandler: (((Bool) -> Void) -> Void)?
-    func stopTracking(trackable: Trackable, onSuccess: @escaping (Bool) -> Void, onError: @escaping ErrorHandler) {
-        stopTrackingCalled = true
-        stopTrackingParamTrackable = trackable
-        stopTrackingParamOnSuccess = onSuccess
-        stopTrackingParamOnError = onError
-
-        stopTrackingOnSuccessCompletionHandler?(onSuccess)
-        stopTrackingOnErrorCompletionHandler?(onError)
-    }
-
     var sendEnhancedAssetLocationUpdateCalled: Bool = false
     var sendEnhancedAssetLocationUpdateParamLocationUpdate: EnhancedLocationUpdate?
     var sendEnhancedAssetLocationUpdateParamTrackable: Trackable?
-    var sendEnhancedAssetLocationUpdateParamCompletion: ((Error?) -> Void)?
-    func sendEnhancedAssetLocationUpdate(locationUpdate: EnhancedLocationUpdate, forTrackable trackable: Trackable, completion: ((Error?) -> Void)?) {
+    var sendEnhancedAssetLocationUpdateParamCompletion: ResultHandler<Void>?
+    func sendEnhancedAssetLocationUpdate(locationUpdate: EnhancedLocationUpdate, forTrackable trackable: Trackable, completion: ResultHandler<Void>?) {
         sendEnhancedAssetLocationUpdateCalled = true
         sendEnhancedAssetLocationUpdateParamLocationUpdate = locationUpdate
         sendEnhancedAssetLocationUpdateParamTrackable = trackable
