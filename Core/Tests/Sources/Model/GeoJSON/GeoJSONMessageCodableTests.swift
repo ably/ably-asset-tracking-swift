1cec205ca745aeb03c497a6c0378633da79e1921
Robert Kuraj <robert.kuraj@applandeo.pl>
2021-02-25 13:49:03 +0100

    [ATS-100] Cover GeoJson classes by tests.

---
+++ Core/Tests/Sources/Model/GeoJSON/GeoJSONMessageCodableTests.swift
@@ -0,0 +1,99 @@
+import XCTest
+import CoreLocation
+@testable import Core
+
+// GeoJSONMessage test also GeoJSONGeometry and GeoJSONProperties
+class GeoJSONMessageCodableTests: XCTestCase {
+    private func jsonMessage(isValid: Bool? = true, type: GeoJSONType? = nil) -> String {
+        return """
+        {
+            "type": "\(type?.rawValue ?? GeoJSONType.feature.rawValue)",
+            "geometry": {
+                "type": "Point",
+                "coordinates": \(isValid ?? true ? [1.0, 2.0, 3.0] : [])
+            },
+            "properties": {
+                "accuracyHorizontal": 1.0,
+                "accuracyVertical": 2.0,
+                "bearing": 3.0,
+                "speed": 4.0,
+                "time": 1234567890.0,
+                "floor": 6,
+                "accuracySpeed": 7.0,
+                "accuracyBearing": 8.0
+            }
+        }
+        """
+    }
+
+    func testEncodedJSON() throws {
+        let json = jsonMessage(isValid: true)
+        let data = json.data(using: .utf8)!
+        let message = try! JSONDecoder().decode(GeoJSONMessage.self, from: data)
+
+        let encodedData = try! JSONEncoder().encode(message)
+        let encodedString = String(data: encodedData, encoding: .utf8)!
+
+        let decodedData = encodedString.data(using: .utf8)!
+        let decodedMessage =  try! JSONDecoder().decode(GeoJSONMessage.self, from: decodedData)
+        
+        XCTAssertEqual(message.type, decodedMessage.type)
+
+        XCTAssertEqual(message.geometry.type, decodedMessage.geometry.type)
+        XCTAssertEqual(message.geometry.longitude, decodedMessage.geometry.longitude, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.geometry.latitude, decodedMessage.geometry.latitude, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.geometry.altitude, decodedMessage.geometry.altitude, accuracy: .ulpOfOne)
+    
+        XCTAssertEqual(message.properties.accuracyHorizontal, decodedMessage.properties.accuracyHorizontal, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.accuracyVertical!, decodedMessage.properties.accuracyVertical!, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.bearing!, decodedMessage.properties.bearing!, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.speed!, decodedMessage.properties.speed!, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.time, decodedMessage.properties.time, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.floor, decodedMessage.properties.floor)
+        XCTAssertEqual(message.properties.accuracySpeed!, decodedMessage.properties.accuracySpeed!, accuracy: .ulpOfOne)
+        XCTAssertEqual(message.properties.accuracyBearing!, decodedMessage.properties.accuracyBearing!, accuracy: .ulpOfOne)
+    }
+    
+    func testGeoJsonMessageFromJson_InvalidJson() {
+        let json = jsonMessage(isValid: false)
+        let data = json.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(GeoJSONMessage.self, from: data))
+    }
+    
+    func testGeoJsonMessageFromJson_ValidJson() {
+        let json = jsonMessage(isValid: true)
+        let data = json.data(using: .utf8)!
+        XCTAssertNoThrow(try JSONDecoder().decode(GeoJSONMessage.self, from: data))
+    }
+    
+    func testGeoJsonMessageFromJson_ValidJson_CheckValues() throws {
+        let json = jsonMessage(isValid: true)
+        let data = json.data(using: .utf8)!
+        let message = try JSONDecoder().decode(GeoJSONMessage.self, from: data)
+        
+        XCTAssertEqual(message.type, .feature)
+        
+        XCTAssertEqual(message.geometry.longitude, 1.0)
+        XCTAssertEqual(message.geometry.latitude, 2.0)
+        XCTAssertEqual(message.geometry.altitude, 3.0)
+        
+        XCTAssertEqual(message.properties.accuracyHorizontal, 1.0)
+        XCTAssertEqual(message.properties.accuracyVertical, 2.0)
+        XCTAssertEqual(message.properties.bearing, 3.0)
+        XCTAssertEqual(message.properties.speed, 4.0)
+        XCTAssertEqual(message.properties.time, 1234567890.0)
+        XCTAssertEqual(message.properties.floor, 6)
+        XCTAssertEqual(message.properties.accuracySpeed, 7.0)
+        XCTAssertEqual(message.properties.accuracyBearing, 8.0)
+    }
+    
+    // MARK: Message type
+    
+    func testGeoJsonMessageFromJson_Type() throws {
+        let type = GeoJSONType.point
+        let json = jsonMessage(isValid: true, type: type)
+        let data = json.data(using: .utf8)!
+        let message = try JSONDecoder().decode(GeoJSONMessage.self, from: data)
+        XCTAssertEqual(message.type, type)
+    }
+}
---
+++ Core/Tests/Sources/Model/GeoJSON/GeoJSONPropertiesCodableTests.swift
@@ -0,0 +1,143 @@
+import XCTest
+
+@testable import Core
+
+class GeoJSONPropertiesCodableTests: XCTestCase {
+    private func propertiesJson(horizontalAccuracy: Double? = nil, accuracyVertical: Double? = nil, bearing: Double? = nil, accuracyBearing: Double? = nil, speed: Double? = nil, accuracySpeed: Double? = nil, timestamp: Date? = nil) -> String {
+        return """
+            {
+                "accuracyHorizontal": \(horizontalAccuracy ?? 1.0),
+                "accuracyVertical": \(accuracyVertical ?? 2.0),
+                "bearing": \(bearing ?? 3.0),
+                "accuracyBearing": \(accuracyBearing ?? 4.0),
+                "speed": \(speed ?? 5.0),
+                "accuracySpeed": \(accuracySpeed ?? 6.0),
+                "floor": 7,
+                "time": \(timestamp?.timeIntervalSince1970 ?? 1234567890.0)
+            }
+        """
+    }
+    
+    // MARK: GeoJSONProperties Encoding and Decoding
+    
+    func testGeoJsonPropertiesFromJsonCoding() throws {
+        let data = propertiesJson().data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        
+        let encodedProperties = try JSONEncoder().encode(properties)
+        let encodedJson = String(data: encodedProperties, encoding: .utf8)!
+        
+        let decodedData = encodedJson.data(using: .utf8)!
+        let decodedProperties = try JSONDecoder().decode(GeoJSONProperties.self, from: decodedData)
+        
+        XCTAssertEqual(properties.accuracyHorizontal, decodedProperties.accuracyHorizontal)
+        XCTAssertEqual(properties.accuracyVertical, decodedProperties.accuracyVertical)
+        XCTAssertEqual(properties.bearing, decodedProperties.bearing)
+        XCTAssertEqual(properties.accuracyBearing, decodedProperties.accuracyBearing)
+        XCTAssertEqual(properties.speed, decodedProperties.speed)
+        XCTAssertEqual(properties.accuracySpeed, decodedProperties.accuracySpeed)
+        XCTAssertEqual(properties.time, decodedProperties.time)
+        XCTAssertEqual(properties.floor, decodedProperties.floor)
+    }
+    
+    // MARK: - Horizontal accuracy
+    
+    func testGeoJsonPropertiesFromJson_InvalidHorizontalAccuracy_LessThanZero() throws {
+        let data = propertiesJson(horizontalAccuracy: -1.0).data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(GeoJSONProperties.self, from: data))
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidHorizontalAccuracy_EqualZero() throws {
+        let data = propertiesJson(horizontalAccuracy: 0.0).data(using: .utf8)!
+        XCTAssertNoThrow(try JSONDecoder().decode(GeoJSONProperties.self, from: data))
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidHorizontalAccuracy_MoreThanZero() throws {
+        let data = propertiesJson(horizontalAccuracy: 10.0).data(using: .utf8)!
+        XCTAssertNoThrow(try JSONDecoder().decode(GeoJSONProperties.self, from: data))
+    }
+    
+    // MARK: - Vertical accuracy
+    
+    func testGeoJsonPropertiesFromJson_InvalidVerticalAccuracy_LessThanZero() throws {
+        let data = propertiesJson(accuracyVertical: -1.0).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertNil(properties.accuracyVertical)
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidVerticalAccuracy() throws {
+        let accuracyVertical = 1.0
+        let data = propertiesJson(accuracyVertical: accuracyVertical).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.accuracyVertical, accuracyVertical)
+    }
+    
+    // MARK: - Bearing accuracy
+    
+    func testGeoJsonPropertiesFromJson_InvalidBearingAccuracy_LessThanZero() throws {
+        let data = propertiesJson(accuracyBearing: -1.0).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertNil(properties.accuracyBearing)
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidBearingAccuracy() throws {
+        let accuracyBearing = 1.0
+        let data = propertiesJson(accuracyBearing: accuracyBearing).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.accuracyBearing, accuracyBearing)
+    }
+    
+    // MARK: - Speed accuracy
+    
+    func testGeoJsonPropertiesFromJson_InvalidSpeedAccuracy_LessThanZero() throws {
+        let data = propertiesJson(accuracySpeed: -1.0).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertNil(properties.accuracySpeed)
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidSpeedAccuracy() throws {
+        let accuracySpeed = 1.0
+        let data = propertiesJson(accuracySpeed: accuracySpeed).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.accuracySpeed, accuracySpeed)
+    }
+    
+    // MARK: - Bearing
+    
+    func testGeoJsonPropertiesFromJson_InvalidBearing_LessThanZero() throws {
+        let data = propertiesJson(bearing: -1.0).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertNil(properties.bearing)
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidBearing() throws {
+        let bearing = 1.0
+        let data = propertiesJson(bearing: bearing).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.bearing, bearing)
+    }
+    
+    // MARK: - Speed
+    
+    func testGeoJsonPropertiesFromJson_InvalidSpeed_LessThanZero() throws {
+        let data = propertiesJson(speed: -1.0).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertNil(properties.speed)
+    }
+    
+    func testGeoJsonPropertiesFromJson_ValidSpeed() throws {
+        let speed = 1.0
+        let data = propertiesJson(speed: speed).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.speed, speed)
+    }
+    
+    // MARK: - Timestamp
+    
+    func testGeoJsonPropertiesFromJson_Timestamp() throws {
+        let timestamp = Date()
+        let data = propertiesJson(timestamp: timestamp).data(using: .utf8)!
+        let properties = try JSONDecoder().decode(GeoJSONProperties.self, from: data)
+        XCTAssertEqual(properties.time, timestamp.timeIntervalSince1970)
+    }
+}
