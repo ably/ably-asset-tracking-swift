5d2962cf313b4132e2f2f9cc62fa9d58d416b1d0
Quintin Willison <q@qwuk.net>
2021-03-30 09:32:43 +0100

    Add script invoking sourcekitten and jazzy to generate HTML docs bundle for API reference.

---
+++ jazzy/build.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+# This script is designed to be run from the root of this repository with:
+# ./jazzy/build.sh
+if [ ! -d "jazzy" ]; then
+  echo "Cannot find jazzy folder. This script must be run from repository root." >&2
+  exit 1
+fi
+
+mkdir -p jazzy/build
+
+# based on the approach here:
+# https://github.com/realm/jazzy/issues/1194#issuecomment-633012820
+
+# Requires sourcekitten, which can be installed via Homebrew.
+# https://github.com/jpsim/SourceKitten
+
+# If we don't specify '-destination' then we get xcodebuild error with:
+# Reason: The run destination My Mac is not valid for Running the scheme 'Publisher'.
+
+sourcekitten doc \
+  --module-name Core \
+  -- \
+  -workspace ./AblyAssetTracking.xcworkspace \
+  -scheme Core \
+  -destination generic/platform=iOS \
+  > jazzy/build/core.json
+
+sourcekitten doc \
+  --module-name Publisher \
+  -- \
+  -workspace ./AblyAssetTracking.xcworkspace \
+  -scheme Publisher \
+  -destination generic/platform=iOS \
+  > jazzy/build/publisher.json
+
+sourcekitten doc \
+  --module-name Subscriber \
+  -- \
+  -workspace ./AblyAssetTracking.xcworkspace \
+  -scheme Subscriber \
+  -destination generic/platform=iOS \
+  > jazzy/build/subscriber.json
+
+bundle exec jazzy --config jazzy/config.yml
