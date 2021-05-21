c18aa94ba886a32421ce82749a98c234516c8136
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-05-21 19:38:24 +0100

    Rename variable for linter

--- Core/Sources/AblyExtensions/ARTRealtimeChannels+Extensions.swift
+++ Core/Sources/AblyExtensions/ARTRealtimeChannels+Extensions.swift
@@ -1,10 +1,10 @@
 import Ably
 
-let CHANNEL_NAME_PREFIX = "tracking"
+private let channelNamePrefix = "tracking"
 
 extension ARTRealtimeChannels {
     public func getChannelFor(trackingId: String, options: ARTRealtimeChannelOptions? = nil) -> ARTRealtimeChannel {
-        let segments = [CHANNEL_NAME_PREFIX, trackingId]
+        let segments = [channelNamePrefix, trackingId]
         let channelName = segments.joined(separator: ":")
         if let options = options {
             return self.get(channelName, options: options);
