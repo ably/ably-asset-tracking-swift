bc7cc02c28c6ea7588d0abd4e2244b4e07c2306b
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-05-23 09:52:03 +0100

    Add files to build phases of Publisher and Subscriber targets

--- Core/Sources/AblyExtensions/ARTRealtimeChannels+Extensions.swift
+++ Core/Sources/AblyExtensions/ARTRealtimeChannels+Extensions.swift
@@ -3,7 +3,7 @@ import Ably
 private let channelNamePrefix = "tracking"
 
 extension ARTRealtimeChannels {
-    public func getChannelFor(trackingId: String, options: ARTRealtimeChannelOptions? = nil) -> ARTRealtimeChannel {
+    func getChannelFor(trackingId: String, options: ARTRealtimeChannelOptions? = nil) -> ARTRealtimeChannel {
         let segments = [channelNamePrefix, trackingId]
         let channelName = segments.joined(separator: ":")
         if let options = options {
