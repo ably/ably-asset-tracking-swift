ce627dcea504709b2a45355510e1cfa2c2992eac
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-06-22 10:06:25 +0100

    Delete unused test cases and files

--- Tests/SystemTests/PublisherSubscriberSystemTests.swift
+++ Tests/SystemTests/PublisherSubscriberSystemTests.swift
@@ -1,4 +1,7 @@
 import XCTest
+import AblyAssetTrackingCore
+import AblyAssetTrackingPublisher
+import AblyAssetTrackingSubscriber
 
 class PublisherSubscriber: XCTestCase {
 
@@ -14,12 +17,4 @@ class PublisherSubscriber: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
