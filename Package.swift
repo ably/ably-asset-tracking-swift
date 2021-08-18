904a0319b735b5a8ca678c786a9fb9aca20de0cf
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-08-18 12:27:08 +0100

    Use latest ably-cocoa release, 1.2.6

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(name: "MapboxNavigation", url: "https://github.com/mapbox/mapbox-navigation-ios.git", .exact("2.0.0-beta.22")),
         .package(url: "https://github.com/realm/SwiftLint", from: "0.43.1"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
-        .package(url: "https://github.com/ably/ably-cocoa", from: "1.2.5")
+        .package(url: "https://github.com/ably/ably-cocoa", from: "1.2.6")
     ],
     targets: [
         //
