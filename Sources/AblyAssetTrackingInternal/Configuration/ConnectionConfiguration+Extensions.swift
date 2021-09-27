f648d1b570679bb8e5033cd9407f34ee4b8f7cc4
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-09-27 08:29:19 +0200

    Add agent header

--- Sources/AblyAssetTrackingInternal/Configuration/ConnectionConfiguration+Extensions.swift
+++ Sources/AblyAssetTrackingInternal/Configuration/ConnectionConfiguration+Extensions.swift
@@ -5,6 +5,7 @@ extension ConnectionConfiguration {
 
     public func getClientOptions() -> ARTClientOptions {
         let clientOptions = ARTClientOptions(key: apiKey)
+        clientOptions.addAgent("ably-asset-tracking-swift", version: Version.currentVersion())
         clientOptions.clientId = clientId
         return clientOptions
     }
