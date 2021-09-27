c2187bac4ec3f753ed2fdb4070d67adb7786059d
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-09-27 09:08:59 +0200

    Change library version access

--- Sources/AblyAssetTrackingInternal/Configuration/ConnectionConfiguration+Extensions.swift
+++ Sources/AblyAssetTrackingInternal/Configuration/ConnectionConfiguration+Extensions.swift
@@ -18,7 +18,7 @@ extension ConnectionConfiguration {
         } else {
             clientOptions.key = apiKey
         }
-        clientOptions.addAgent("ably-asset-tracking-swift", version: Version.currentVersion())
+        clientOptions.addAgent("ably-asset-tracking-swift", version: Version.libraryVersion)
         clientOptions.clientId = clientId
         
         return clientOptions
