a0e20a90b35bba99c8359edc34f47cb8edb8284d
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-11-02 10:31:44 +0100

    Add Secrets example to SystemTests

---
+++ Tests/SystemTests/Example.Secrets.swift
@@ -0,0 +1,4 @@
+struct Secrets {
+	static let ablyApiKey = "61NQ-g.e66C0w:waj9spejaDuFlEGk"
+	static let mapboxAccessToken = "pk.eyJ1IjoibC1zenlzemtvd3NraSIsImEiOiJja3U4NjBpbWUweTRnMnNwYTFycjFwYTgwIn0.Blr0GsboqnP3c4_a0D8DUg"
+}
