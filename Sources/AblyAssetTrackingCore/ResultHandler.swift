bd5d2a36514ce240741329bedec095fe70589c88
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-11-30 11:26:12 +0100

    Change Result/ResultHandler to be visible in both, swift and obj-c

--- Sources/AblyAssetTrackingCore/ResultHandler.swift
+++ Sources/AblyAssetTrackingCore/ResultHandler.swift
@@ -1,10 +1,68 @@
-/**
- Completion handler for operations
- */
-public typealias ResultHandler<T: Any> = (Result<T, ErrorInformation>) -> Void
+import Foundation
 
-public extension Result where Success == Void {
-    static var success: Result {
+public typealias ResultHandler = (ATResult) -> Void
+
+@objc
+public protocol Resultable {
+    var success: Any? { get }
+    var failure: ErrorInformation? { get }
+}
+
+@objcMembers
+public class ATResult: NSObject, Resultable {
+    public let success: Any?
+    public let failure: ErrorInformation?
+    
+    public static var success: ATResult {
+        return .init(success: Void(), failure: nil)
+    }
+    
+    public static func success(_ value: Any?) -> ATResult {
+        return .init(success: value, failure: nil)
+    }
+    
+    public static func failure(_ value: ErrorInformation?) -> ATResult {
+        return .init(success: nil, failure: value)
+    }
+    
+    public init(success: Any? = nil, failure: ErrorInformation? = nil) {
+        self.success = success
+        self.failure = failure
+    }
+    
+    public override init() {
+        self.success = nil
+        self.failure = nil
+        
+        super.init()
+    }
+}
+
+public extension ATResult {
+    enum Unwrapped<T> {
+        case success(T)
+        case failure(ErrorInformation)
+    }
+    
+    var enumUnwrap: Unwrapped<Void> {
+        if let failure = failure {
+            return .failure(failure)
+        }
+        
         return .success(Void())
+    }
+    
+    func enumUnwrap<T>(_:T.Type) -> Unwrapped<T> {
+        if let failure = failure {
+            return .failure(failure)
+        } else if let success = success {
+            if let casted = success as? T {
+                return .success(casted)
+            } else {
+                fatalError("Unable to cast \(type(of: success)) to `\(T.self)`")
+            }
+        }
+        
+        return .success(Void() as! T)
     }
 }
