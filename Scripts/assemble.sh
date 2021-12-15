4d64097a6155a19d3907a52bc445a5dd76a9246d
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-12-15 14:20:04 +0100

    Remove code repetition

--- Scripts/assemble.sh
+++ Scripts/assemble.sh
@@ -1,6 +1,7 @@
 # This script is used to check that "Ably Asset Tracker" lib integration works properly in example apps.
 
 set -e
+set -o pipefail
 
 # Install xcpretty (https://github.com/xcpretty/xcpretty)
 gem install --user-install xcpretty
@@ -10,39 +11,39 @@ echo
 echo '\033[1mBuild: AblyAssetTrackingSubscriber Library\033[0m'
 echo
 
-set -o pipefail & xcodebuild -scheme "AblyAssetTrackingPublisher" -destination "generic/platform=iOS" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
+xcodebuild -scheme "AblyAssetTrackingPublisher" -destination "generic/platform=iOS" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
 
 # Build AblyAssetTrackingPublisher lib
 echo
 echo '\033[1mBuild: AblyAssetTrackingPublisher Library\033[0m'
 echo
 
-set -o pipefail & xcodebuild -scheme "AblyAssetTrackingPublisher" -destination "generic/platform=iOS" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
+xcodebuild -scheme "AblyAssetTrackingPublisher" -destination "generic/platform=iOS" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
 
 # Publisher Example (swift)
 echo
 echo '\033[1mBuild: PublisherExample\033[0m'
 echo
 
-set -o pipefail & xcodebuild build -scheme "PublisherExample" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
+xcodebuild build -scheme "PublisherExample" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
 
 # Publisher Example (objective-c)
 echo
 echo '\033[1mBuild: PublisherExampleObjectiveC\033[0m'
 echo
 
-set -o pipefail & xcodebuild build -scheme "PublisherExampleObjectiveC" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
+xcodebuild build -scheme "PublisherExampleObjectiveC" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
 
 # Subscriber Example (swift)
 echo
 echo '\033[1mBuild: SubscriberExample\033[0m'
 echo
 
-set -o pipefail & xcodebuild build -scheme "SubscriberExample" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
+xcodebuild build -scheme "SubscriberExample" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
 
 # Subscriber Example (objective-c)
 echo
 echo '\033[1mBuild: SubscriberExampleObjectiveC\033[0m'
 echo
 
-set -o pipefail & xcodebuild build -scheme "SubscriberExampleObjectiveC" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
+xcodebuild build -scheme "SubscriberExampleObjectiveC" -workspace "./Examples/AblyAssetTracking.xcworkspace" -sdk "iphonesimulator" -configuration "Debug" | xcpretty
