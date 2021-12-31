f39206d8ee8a50eb0a116db6bd819ad03db1d8ce
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2021-12-31 08:53:04 +0100

    Add Google Crashlytics support for the example app

--- Examples/PublisherExample/Sources/AppDelegate.swift
+++ Examples/PublisherExample/Sources/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Logging
+import Firebase
 
 let logger: Logger = Logger(label: "com.ably.tracking.PublisherExample")
 
@@ -8,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        setupCrashlytics()
         setupLogger()
 
         let window = UIWindow(frame: UIScreen.main.bounds)
@@ -27,5 +30,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             handler.logLevel = .debug
             return handler
         }
+    }
+    
+    private func setupCrashlytics() {
+        guard let config = Bundle(for: Self.self).path(forResource: "GoogleService-Info", ofType: "plist") else {
+            print("""
+            [Crashlyics Info]
+                Firebase Crashlytics won't work.
+                Please add "GoogleService-Info.plist" to the project if you want to use the Crashlytics reporter.
+                You can register your app here https://console.firebase.google.com/
+            
+            """)
+            return
+        }
+        
+        guard let options = FirebaseOptions(contentsOfFile: config) else {
+            return
+        }
+        
+        FirebaseApp.configure(options: options)
     }
 }
