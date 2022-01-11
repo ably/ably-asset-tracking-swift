969c2a5f7c9db03914178455cd2409553b52d079
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2022-01-11 14:57:47 +0100

    Add public initialiser for `LocationCoordinate` struct

--- Sources/AblyAssetTrackingCore/Model/LocationCoordinate.swift
+++ Sources/AblyAssetTrackingCore/Model/LocationCoordinate.swift
@@ -11,4 +11,9 @@ public struct LocationCoordinate: Equatable {
      Longitude of the location in degrees.
      */
     public let longitude: Double
+    
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
