fb521295c43dd27c4e02abe02b116ccc7d6ec5de
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2022-02-08 14:43:12 +0100

    Add Ably wrapper protocols

---
+++ Sources/AblyAssetTrackingInternal/AblyWrapper/AblyCommon.swift
@@ -0,0 +1,83 @@
+import AblyAssetTrackingCore
+import Logging
+
+public protocol AblyCommon {
+    /**
+     Observe for the `ARTRealtime` state updates.
+     */
+    func subscribeForAblyStateChange()
+    
+    /**
+     Observe  for the channel state updates.
+     
+     Subscription should be able  only when there's an existing channel for the `trackable.id`
+     
+     - Parameter trackable: The `Trackable` object
+     */
+    func subscribeForChannelStateChange(trackable: Trackable)
+    
+    /**
+     Observe  for the presence messages that are received from the channel's presence.
+     
+     Subscription should be able  only when there's an existing channel for the `trackable.id`
+     
+     - Parameter trackable:  The `Trackable` object
+     */
+    func subscribeForPresenceMessages(trackable: Trackable)
+    
+    /**
+     Joins the presence of the channel for the given `trackableId` and add it to the connected channels.
+     
+     If successfully joined the presence then the channel is added to the connected channels.
+     If a channel for the given `trackableId` exists then it just calls `completion` with success.
+     
+     - Parameter trackableId:   The ID of the trackable channel.
+     - Parameter presenceData:  The data that will be send via the presence channel.
+     - Parameter useRewind:     If set to true then after connecting the channel will replay the last event that was sent in it.
+     - Parameter completion:    The closure that will be called when connecting completes. If something goes wrong it will be called with `error` object.
+     */
+    func connect(trackableId: String, presenceData: PresenceData, useRewind: Bool, completion: @escaping ResultHandler<Void>)
+    
+    /**
+     Removes the `trackableId` channel from the connected channels and leaves the presence of that channel.
+     
+     If a channel for the given `trackableId` doesn't exist then it just calls `completion` with success.
+     
+     - Parameter trackableId:   The ID of the trackable channel.
+     - Parameter presenceData:  The data that will be send via the presence channel.
+     - Parameter completion:    The closure that will be called when disconnecting completes. If something goes wrong it will be called with `error` object.
+     */
+    func disconnect(trackableId: String, presenceData: PresenceData, completion: @escaping ResultHandler<Bool>)
+    
+    /**
+     Cleanups and closes all the connected channels and their presence. In the end closes Ably connection.
+     
+     - Parameter presenceData:  The data that will be send via the presence channel.
+     - Parameter completion:    The closure that will be called when `Ably` connection state will change to `closed` or `failed`.
+     */
+    func close(presenceData: PresenceData, completion: @escaping ResultHandler<Void>)
+    
+    /**
+     Common required initialiser
+     
+     - Parameter configuration: The `ConnectionConfiguration` object
+     - Parameter mode:          The mode in which `Ably` wrapper is initialise. Available modes: `subscribe`, `publish`
+     - Parameter logger:        The object of the `Logger` introduced in `swift-log` package
+     
+     */
+    init(configuration: ConnectionConfiguration, mode: AblyMode, logger: Logger)
+}
+
+/**
+ The `AblyMode` is an `OptionSet` which defines how `AblyWrapper` should works: as a `publisher`, as a `subscriber` or both.
+ */
+public struct AblyMode: OptionSet {
+    public let rawValue: Int
+    
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    public static let publish = AblyMode(rawValue: 1 << 0)
+    public static let subscribe = AblyMode(rawValue: 1 << 1)
+}
---
+++ Sources/AblyAssetTrackingInternal/AblyWrapper/AblyPublisher.swift
@@ -0,0 +1,73 @@
+import Foundation
+import AblyAssetTrackingCore
+
+public protocol AblyPublisherServiceDelegate: AnyObject {
+    /**
+     Tells the delegate that `Ably` client connection state changed.
+     
+     - Parameter sender:    The `AblyPublisher` object which is delegating the change.
+     - Parameter state:     The `ConnectionState` object
+     */
+    func publisherService(sender: AblyPublisher, didChangeConnectionState state: ConnectionState)
+    
+    /**
+     Tells the delegate that channel connection state changed.
+     
+     - Parameter sender:        The `AblyPublisher` object which is delegating the change.
+     - Parameter state:         The `ConnectionState` object
+     - Parameter trackable:     The `Trackable` object affected by the change
+     */
+    func publisherService(sender: AblyPublisher, didChangeChannelConnectionState state: ConnectionState, forTrackable trackable: Trackable)
+    
+    /**
+     Tells the delegate that an error occurred.
+     
+     This is a generic delegate method and can be called from any method in the `Ably` wrapper
+     
+     - Parameter sender:        The `AblyPublisher` object which is delegating the change.
+     - Parameter error:         The `ErrorInformation` object that contains info about error.
+     */
+    func publisherService(sender: AblyPublisher, didFailWithError error: ErrorInformation)
+    
+    /**
+     Tells the delegate that channel presence data was changed.
+     
+     - Parameter sender:        The `AblyPublisher` object which is delegating the change.
+     - Parameter presence:      The `Presence` object affected by the change.
+     - Parameter trackable:     The `Trackable` object affected by the change.
+     - Parameter presenceData:  The `PresenceData` object that contains info related to presence change.
+     - Parameter clientId:      The `Ably` client identifier.
+     */
+    func publisherService(
+        sender: AblyPublisher,
+        didReceivePresenceUpdate presence: Presence,
+        forTrackable trackable: Trackable,
+        presenceData: PresenceData,
+        clientId: String
+    )
+}
+
+public protocol AblyPublisher: AblyCommon {
+    /**
+     The delegate of the `Ably` wrapper object.
+     
+     The methods declared by the `AblyPublisherServiceDelegate` protocol allow the adopting delegate to respond to messages from the `Ably` wrapper class..
+     */
+    var publisherDelegate: AblyPublisherServiceDelegate? { get set }
+    
+    /**
+     Sends an enhanced location update to the channel.
+     
+     Should be called only when there's an existing channel for the `trackable.id`.
+     If a channel for the `trackable.id` doesn't exist then it just calls `completion` with success.
+     
+     - Parameter locationUpdate:     The location update that is sent to the channel.
+     - Parameter trackable:          The `Trackable` instance.
+     - Parameter completion:         The closure that will be called when sending completes. If something goes wrong it will be called with an `error` object.
+     */
+    func sendEnhancedLocation(
+        locationUpdate: EnhancedLocationUpdate,
+        trackable: Trackable,
+        completion: ResultHandler<Void>?
+    )
+}
