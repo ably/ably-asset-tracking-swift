775b76a4bc463fcc50276182e147d87aa126cab4
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2022-02-23 11:26:02 +0100

    Extend `PresenceData` model if raw locations info are enabled or not

--- Sources/AblyAssetTrackingInternal/PresenceData.swift
+++ Sources/AblyAssetTrackingInternal/PresenceData.swift
@@ -4,14 +4,17 @@ import AblyAssetTrackingCore
 public struct PresenceData: Codable {
     public let type: ClientType
     public let resolution: Resolution?
+    public let rawLocations: Bool?
 
-    public init(type: ClientType) {
+    public init(type: ClientType, rawLocations: Bool? = nil) {
         self.type = type
         self.resolution = nil
+        self.rawLocations = rawLocations
     }
 
-    public init(type: ClientType, resolution: Resolution?) {
+    public init(type: ClientType, resolution: Resolution?, rawLocations: Bool? = nil) {
         self.type = type
         self.resolution = resolution
+        self.rawLocations = rawLocations
     }
 }
