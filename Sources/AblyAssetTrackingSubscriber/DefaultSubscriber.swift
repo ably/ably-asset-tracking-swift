43f509fb4aca4b3bb6b95945bec22b1a0ef8741a
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2022-03-07 08:33:59 +0100

    Change `CLLocation` to custom `Location` object

--- Sources/AblyAssetTrackingSubscriber/DefaultSubscriber.swift
+++ Sources/AblyAssetTrackingSubscriber/DefaultSubscriber.swift
@@ -292,7 +292,7 @@ extension DefaultSubscriber: AblySubscriberServiceDelegate {
         callback(event: DelegateRawLocationReceivedEvent(location: location))
     }
     
-    func subscriberService(sender: AblySubscriber, didReceiveEnhancedLocation location: CLLocation) {
+    func subscriberService(sender: AblySubscriber, didReceiveEnhancedLocation location: Location) {
         logger.debug("subscriberService.didReceiveEnhancedLocation.", source: String(describing: Self.self))
         callback(event: DelegateEnhancedLocationReceivedEvent(location: location))
     }
