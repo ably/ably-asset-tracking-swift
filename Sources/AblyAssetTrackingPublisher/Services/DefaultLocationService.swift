32649358bc1c17e329490bc7f15af777e0cc3d5e
Lawrence Forooghian <lawrence@forooghian.com>
2022-10-04 15:02:35 -0300

    Log changes over time of locations received from Mapbox
    This addresses the issues of 8fc5135, making it easier to investigate #377.

--- Sources/AblyAssetTrackingPublisher/Services/DefaultLocationService.swift
+++ Sources/AblyAssetTrackingPublisher/Services/DefaultLocationService.swift
@@ -7,6 +7,8 @@ class DefaultLocationService: LocationService {
     private let locationManager: PassiveLocationManager
     private let replayLocationManager: ReplayLocationManager?
     private let logHandler: LogHandler?
+    // Used just for logging purposes, to assess the changes over time in the sequence of received locations.
+    private var lastReceivedLocations: (location: CLLocation, rawLocation: CLLocation)?
 
     weak var delegate: LocationServiceDelegate?
 
@@ -54,6 +56,16 @@ extension DefaultLocationService: PassiveLocationManagerDelegate {
     
     func passiveLocationManager(_ manager: PassiveLocationManager, didUpdateLocation location: CLLocation, rawLocation: CLLocation) {
         logHandler?.verbose(message: "\(String(describing: Self.self)), passiveLocationManager(\(manager), didUpdateLocation: \(String(reflecting: location)), rawLocation: \(String(reflecting: rawLocation)))", error: nil)
+
+        if let lastReceivedLocations = lastReceivedLocations {
+            let locationDifferences = LocationDifferenceCalculator.calculateDifferences(first: location, second: lastReceivedLocations.location)
+            logHandler?.verbose(message: "\(String(describing: Self.self)), Differences between location and last received location: \(String(reflecting: locationDifferences))", error: nil)
+            let rawLocationDifferences = LocationDifferenceCalculator.calculateDifferences(first: rawLocation, second: lastReceivedLocations.rawLocation)
+            logHandler?.verbose(message: "\(String(describing: Self.self)), Differences between rawLocation and last received rawLocation: \(String(reflecting: rawLocationDifferences))", error: nil)
+        }
+        
+        lastReceivedLocations = (location: location, rawLocation: rawLocation)
+        
         delegate?.locationService(sender: self, didUpdateRawLocationUpdate: RawLocationUpdate(location: rawLocation.toLocation()))
         delegate?.locationService(sender: self, didUpdateEnhancedLocationUpdate: EnhancedLocationUpdate(location: location.toLocation()))
     }
