9c44d5da2cf4f1f37a3c2295047346fd4c007945
Lawrence Forooghian <lawrence@forooghian.com>
2022-10-12 20:25:25 -0300

    Publisher example: Add helper for creating Alert from ErrorInformation
    Very basic - at some point we’ll want to extend it to make more use of
    the fields from ErrorInformation.

---
+++ Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Helper/Alert+ErrorInformation.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+import AblyAssetTrackingCore
+
+extension Alert {
+    init(title: String, errorInformation: ErrorInformation?) {
+        self.init(title: Text(title),
+                  message: Text(errorInformation?.message ?? ""))
+    }
+}
