512729b94aa8e5ae8d72870bd9a2f2da18e2a7be
Lawrence Forooghian <lawrence@forooghian.com>
2022-10-12 17:43:13 -0300

    Publisher example: Add button to remove a trackable

--- Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/View/Map/MapView.swift
+++ Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/View/Map/MapView.swift
@@ -5,14 +5,53 @@ import AblyAssetTrackingPublisher
 struct MapView: View {
     var trackableId: String
     @ObservedObject var publisher: ObservablePublisher
+    @Environment(\.presentationMode) private var presentationMode
     
     @StateObject private var viewModel = MapViewModel()
     @StateObject private var locationManager = LocationManager.shared
+    @State private var isRemoving = false
+    @State private var hasRemoved = false
+    @State private var error: ErrorInformation?
+    @State private var showAlert = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 3) {
+        VStack(alignment: .leading, spacing: 10) {
             StackedText(texts: MapViewModel.createViewModel(forConnectionState: publisher.trackables.first { key, _ in key.id == trackableId }?.value.connectionState))
                 .padding(.leading, 10)
+            // This padding is very bad - for some reason, I wasn't able to tap on the "Remove trackable" button without it. I'm probably doing something very very wrong but don't have time to look into it now. See https://github.com/ably/ably-asset-tracking-swift/issues/422
+                .padding(.bottom, 25)
+            
+            HStack {
+                Spacer()
+                Button {
+                    isRemoving = true
+                    let trackable = publisher.trackables.first { key, _ in key.id == trackableId }?.key
+                    guard let trackable = trackable else { return }
+                    publisher.remove(trackable: trackable) { result in
+                        isRemoving = false
+                        switch result {
+                        case .success:
+                            hasRemoved = true
+                            presentationMode.wrappedValue.dismiss()
+                        case .failure(let error):
+                            self.error = error
+                            showAlert = true
+                        }
+                    }
+                } label: {
+                    Text("Remove trackable")
+                        .accentColor(.red)
+                }
+                .disabled(isRemoving || hasRemoved)
+                .alert(isPresented: $showAlert) {
+                    Alert(title: "Failed to stop publisher",
+                          errorInformation: error)
+                }
+                .padding(.trailing)
+                ProgressView()
+                    .opacity(isRemoving ? 1 : 0)
+                Spacer()
+            }
             
             ZStack(alignment: .bottomTrailing) {
                 if viewModel.useMapboxMap {
