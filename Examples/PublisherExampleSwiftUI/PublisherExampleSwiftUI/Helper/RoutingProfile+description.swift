a4c32b7bacd0cac786483e6d5ec32db30c5ff753
Jakub Jankowski <j.jankowski@nomtek.com>
2022-10-18 14:15:29 +0200

    Add extension to the RoutingProfile enum in the Publisher example app

---
+++ Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Helper/RoutingProfile+description.swift
@@ -0,0 +1,33 @@
+import AblyAssetTrackingPublisher
+
+extension RoutingProfile {
+    
+    func description() -> String {
+        switch self {
+        case .driving:
+            return "driving"
+        case .drivingTraffic:
+            return "drivingTraffic"
+        case .walking:
+            return "walking"
+        case .cycling:
+            return "cycling"
+        }
+    }
+    
+    static func fromDescription(description: String) -> RoutingProfile {
+        if description == "driving" {
+            return .driving
+        }
+        if description == "drivingTraffic" {
+            return .drivingTraffic
+        }
+        if description == "walking" {
+            return .walking
+        }
+        if description == "cycling" {
+            return .cycling
+        }
+        return .driving
+    }
+}
