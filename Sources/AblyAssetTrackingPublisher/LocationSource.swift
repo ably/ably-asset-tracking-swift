fff795cfcfd8960fb50c9baa4483c4d5d7432d27
Lawrence Forooghian <lawrence@forooghian.com>
2022-10-26 14:51:34 -0300

    Remove unused LocationSource.type
    Also deprecate the its type, which shouldn’t have been part of the
    public interface in the first place.

--- Sources/AblyAssetTrackingPublisher/LocationSource.swift
+++ Sources/AblyAssetTrackingPublisher/LocationSource.swift
@@ -1,22 +1,20 @@
 import CoreLocation
 
+@available(*, deprecated, message: "No longer used.")
 public enum LocationSourceType: Int {
     case `default`
     case raw
 }
 
 public class LocationSource {
-    let type: LocationSourceType
     let locationSource: [CLLocation]?
     
     public init(locationSource: [CLLocation]?) {
-        self.type = locationSource != nil ? .raw : .default
         self.locationSource = locationSource
     }
     
     @available(*, deprecated, message: "To use the device’s location, either do not call PublisherBuilder.locationSource(_:), or pass nil to it.")
     public init() {
-        self.type = .default
         self.locationSource = nil
     }
 }
