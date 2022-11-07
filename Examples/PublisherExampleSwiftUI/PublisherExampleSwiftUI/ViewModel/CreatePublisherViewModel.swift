ably/ably-asset-tracking-swift

commit ac2080697a1740020f0f59d861f6b4769018a3e2

Publisher example: Add ability to use a file fetched from S3 as a location source
Closes #410, closes #437.

Co-authored-by: Lawrence Forooghian <lawrence@forooghian.com>

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/ViewModel/CreatePublisherViewModel.swift
@@ -5,6 +5,21 @@ import SwiftUI
 import AblyAssetTrackingPublisher
 
 class CreatePublisherViewModel: ObservableObject {
+    
+    private let s3Helper: S3Helper?
+    
+    private var isS3Available: Bool {
+        s3Helper != nil
+    }
+    
+    init() {
+        do {
+            self.s3Helper = try S3Helper()
+        } catch {
+            self.s3Helper = nil
+        }
+    }
+    
     @Published var areRawLocationsEnabled: Bool = SettingsModel.shared.areRawLocationsEnabled {
         didSet {
             SettingsModel.shared.areRawLocationsEnabled = areRawLocationsEnabled
@@ -26,6 +41,18 @@ class CreatePublisherViewModel: ObservableObject {
     @Published var routingProfile: RoutingProfile = SettingsModel.shared.routingProfile {
         didSet {
             SettingsModel.shared.routingProfile = routingProfile
+        }
+    }
+    
+    @Published var locationSource: LocationSourceOption = SettingsModel.shared.locationSourceOption {
+        didSet {
+            SettingsModel.shared.locationSourceOption = locationSource
+        }
+    }
+    
+    @Published var s3FileName: String? = SettingsModel.shared.s3FileName {
+        didSet {
+            SettingsModel.shared.s3FileName = s3FileName
         }
     }
     
@@ -52,6 +79,14 @@ class CreatePublisherViewModel: ObservableObject {
          RoutingProfile.walking].map { $0.description() }
     }
     
+    var locationSources: [String] {
+        var sources: [LocationSourceOption] = [.phone]
+        if isS3Available {
+            sources.append(.s3File)
+        }
+        return sources.map { $0.description() }
+    }
+    
     func save() {
         if let constantAccuracy = Accuracy(rawValue: constantResolutionAccuracy),
            let constantDisplacement = Double(constantResolutionMinimumDisplacement) {
@@ -61,21 +96,34 @@ class CreatePublisherViewModel: ObservableObject {
         }
     }
     
-    func createPublisher() -> ObservablePublisher {
+    func createPublisher() async throws -> ObservablePublisher {
         let connectionConfiguration = ConnectionConfiguration(apiKey: EnvironmentHelper.ABLY_API_KEY, clientId: "Asset Tracking Publisher Example")
         let resolution = Resolution(accuracy: .balanced, desiredInterval: 5000, minimumDisplacement: 100)
 
         let constantResolution: Resolution? = SettingsModel.shared.isConstantResolutionEnabled ? SettingsModel.shared.constantResolution : nil
         let vehicleProfile = SettingsModel.shared.vehicleProfile
         let routingProfile = SettingsModel.shared.routingProfile
+        let locationSourceOption = SettingsModel.shared.locationSourceOption
         
         let areRawLocationsEnabled = SettingsModel.shared.areRawLocationsEnabled
-                
-        var publisher = try! PublisherFactory.publishers()
+        
+        let locationSource: AblyAssetTrackingPublisher.LocationSource?
+        if let s3FileName = SettingsModel.shared.s3FileName, locationSourceOption == .s3File {
+            guard let s3Helper = s3Helper else {
+                // This ignores the case where we used to have S3 configured on a previous run of the app and still have something hanging around in user defaults, but will ignore that edge case for now.
+                fatalError("Trying to use S3 when it has not been configured. This should not have been allowed by the UI.")
+            }
+            
+            let locationHistoryData = try await s3Helper.downloadHistoryData(fileName: s3FileName)
+            locationSource = .init(locationHistoryData: locationHistoryData)
+        } else {
+            locationSource = nil
+        }
+            
+        var publisher = try PublisherFactory.publishers()
             .connection(connectionConfiguration)
             .mapboxConfiguration(MapboxConfiguration(mapboxKey: EnvironmentHelper.MAPBOX_ACCESS_TOKEN))
-            // Uncomment below line to enable simulated location
-//          .locationSource(.init(locationSource: SimulatedLocations.recordedLocations()))
+            .locationSource(locationSource)
             .routingProfile(routingProfile)
             .resolutionPolicyFactory(DefaultResolutionPolicyFactory(defaultResolution: resolution))
             .rawLocations(enabled: areRawLocationsEnabled)

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI.xcodeproj/project.pbxproj
@@ -29,6 +29,9 @@
 		2144B8212908276F00958144 /* SelectDestinationView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2144B8202908276F00958144 /* SelectDestinationView.swift */; };
 		2144B8252908278B00958144 /* DestinationMapView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 2144B8242908278B00958144 /* DestinationMapView.swift */; };
 		21B6446B28FDC68C00156F94 /* AddTrackableViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21B6446A28FDC68C00156F94 /* AddTrackableViewModel.swift */; };
+		21B6B2A829097EAF001E2FB7 /* LocationSourceOption.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21B6B2A729097EAF001E2FB7 /* LocationSourceOption.swift */; };
+		21B6B2AA29097FEC001E2FB7 /* S3FilesView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21B6B2A929097FEB001E2FB7 /* S3FilesView.swift */; };
+		21B6B2AC2909800B001E2FB7 /* S3FilesViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21B6B2AB2909800B001E2FB7 /* S3FilesViewModel.swift */; };
 		21CF8DFE28F8566D005E7CE6 /* ObservablePublisher.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21CF8DFD28F8566D005E7CE6 /* ObservablePublisher.swift */; };
 		21CF8E0328F875DF005E7CE6 /* SettingsView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21CF8E0228F875DF005E7CE6 /* SettingsView.swift */; };
 		21CF8E0528F875E8005E7CE6 /* SettingsViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21CF8E0428F875E8005E7CE6 /* SettingsViewModel.swift */; };
@@ -39,7 +42,6 @@
 		21F1993228FF40E6004FE626 /* PublisherDetailsView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21F1993128FF40E6004FE626 /* PublisherDetailsView.swift */; };
 		21F1993428FF40F3004FE626 /* ConnectionState+Formatting.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21F1993328FF40F3004FE626 /* ConnectionState+Formatting.swift */; };
 		21F1993628FF41B2004FE626 /* PublisherInfoViewModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 21F1993528FF41B2004FE626 /* PublisherInfoViewModel.swift */; };
-		D511FDD427FED05E00EBF3D6 /* SimulatedLocations.swift in Sources */ = {isa = PBXBuildFile; fileRef = D511FDD327FED05D00EBF3D6 /* SimulatedLocations.swift */; };
 		D544FAF62771B3560015925C /* PublisherExampleSwiftUIApp.swift in Sources */ = {isa = PBXBuildFile; fileRef = D544FAF52771B3560015925C /* PublisherExampleSwiftUIApp.swift */; };
 		D544FAFA2771B3580015925C /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = D544FAF92771B3580015925C /* Assets.xcassets */; };
 		D544FAFD2771B3580015925C /* Preview Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = D544FAFC2771B3580015925C /* Preview Assets.xcassets */; };
@@ -72,6 +74,9 @@
 		2144B8202908276F00958144 /* SelectDestinationView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SelectDestinationView.swift; sourceTree = "<group>"; };
 		2144B8242908278B00958144 /* DestinationMapView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = DestinationMapView.swift; sourceTree = "<group>"; };
 		21B6446A28FDC68C00156F94 /* AddTrackableViewModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AddTrackableViewModel.swift; sourceTree = "<group>"; };
+		21B6B2A729097EAF001E2FB7 /* LocationSourceOption.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = LocationSourceOption.swift; sourceTree = "<group>"; };
+		21B6B2A929097FEB001E2FB7 /* S3FilesView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = S3FilesView.swift; sourceTree = "<group>"; };
+		21B6B2AB2909800B001E2FB7 /* S3FilesViewModel.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = S3FilesViewModel.swift; sourceTree = "<group>"; };
 		21CF8DFD28F8566D005E7CE6 /* ObservablePublisher.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ObservablePublisher.swift; sourceTree = "<group>"; };
 		21CF8E0228F875DF005E7CE6 /* SettingsView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SettingsView.swift; sourceTree = "<group>"; };
 		21CF8E0428F875E8005E7CE6 /* SettingsViewModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SettingsViewModel.swift; sourceTree = "<group>"; };
@@ -82,7 +87,6 @@
 		21F1993128FF40E6004FE626 /* PublisherDetailsView.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = PublisherDetailsView.swift; sourceTree = "<group>"; };
 		21F1993328FF40F3004FE626 /* ConnectionState+Formatting.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = "ConnectionState+Formatting.swift"; sourceTree = "<group>"; };
 		21F1993528FF41B2004FE626 /* PublisherInfoViewModel.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = PublisherInfoViewModel.swift; sourceTree = "<group>"; };
-		D511FDD327FED05D00EBF3D6 /* SimulatedLocations.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SimulatedLocations.swift; sourceTree = "<group>"; };
 		D544FAF22771B3560015925C /* PublisherExampleSwiftUI.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = PublisherExampleSwiftUI.app; sourceTree = BUILT_PRODUCTS_DIR; };
 		D544FAF52771B3560015925C /* PublisherExampleSwiftUIApp.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PublisherExampleSwiftUIApp.swift; sourceTree = "<group>"; };
 		D544FAF92771B3580015925C /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
@@ -243,6 +247,7 @@
 		D544FB202771D8290015925C /* View */ = {
 			isa = PBXGroup;
 			children = (
+				21B6B2A929097FEB001E2FB7 /* S3FilesView.swift */,
 				2144B8202908276F00958144 /* SelectDestinationView.swift */,
 				21F1993128FF40E6004FE626 /* PublisherDetailsView.swift */,
 				21F1993028FF40B7004FE626 /* Previews */,
@@ -265,6 +270,7 @@
 		D544FB45277332670015925C /* ViewModel */ = {
 			isa = PBXGroup;
 			children = (
+				21B6B2AB2909800B001E2FB7 /* S3FilesViewModel.swift */,
 				21F1993528FF41B2004FE626 /* PublisherInfoViewModel.swift */,
 				0CFCA3012891312F00A1A6B5 /* CreatePublisherViewModel.swift */,
 				D544FB46277332760015925C /* MapViewModel.swift */,
@@ -277,6 +283,7 @@
 		D544FB49277333940015925C /* Helper */ = {
 			isa = PBXGroup;
 			children = (
+				21B6B2A729097EAF001E2FB7 /* LocationSourceOption.swift */,
 				2108B758290974B8002778CA /* S3Helper.swift */,
 				21F1993328FF40F3004FE626 /* ConnectionState+Formatting.swift */,
 				0CFCA306289131AB00A1A6B5 /* CloseKeyboardHelper.swift */,
@@ -294,7 +301,6 @@
 			children = (
 				21CF8DFD28F8566D005E7CE6 /* ObservablePublisher.swift */,
 				0CFCA3032891315700A1A6B5 /* SettingsModel.swift */,
-				D511FDD327FED05D00EBF3D6 /* SimulatedLocations.swift */,
 				D5DBAF722774625C008B8DED /* StackedTextModel.swift */,
 			);
 			path = Model;
@@ -390,11 +396,12 @@
 				D544FAF62771B3560015925C /* PublisherExampleSwiftUIApp.swift in Sources */,
 				D5DBAF7527748116008B8DED /* RoutingProfile.swift in Sources */,
 				2144B8212908276F00958144 /* SelectDestinationView.swift in Sources */,
+				21B6B2AA29097FEC001E2FB7 /* S3FilesView.swift in Sources */,
+				21B6B2AC2909800B001E2FB7 /* S3FilesViewModel.swift in Sources */,
 				21B6446B28FDC68C00156F94 /* AddTrackableViewModel.swift in Sources */,
 				D544FB4B277333A20015925C /* Environment.swift in Sources */,
 				21E6EBF328F836FF0057AC7C /* MapboxMap.swift in Sources */,
 				21E6EBF528F837380057AC7C /* MapboxMapViewController.swift in Sources */,
-				D511FDD427FED05E00EBF3D6 /* SimulatedLocations.swift in Sources */,
 				0CFCA2FD289130E700A1A6B5 /* ListItem.swift in Sources */,
 				21E6EBF828F97B200057AC7C /* Map.swift in Sources */,
 				0CFCA30A2891325700A1A6B5 /* AddTrackableView.swift in Sources */,
@@ -415,6 +422,7 @@
 				D5DBAF732774625C008B8DED /* StackedTextModel.swift in Sources */,
 				D544FB47277332760015925C /* MapViewModel.swift in Sources */,
 				0CFCA2FE289130E700A1A6B5 /* CreatePublisherView.swift in Sources */,
+				21B6B2A829097EAF001E2FB7 /* LocationSourceOption.swift in Sources */,
 				0C50478528FECFA000E84038 /* RoutingProfile+description.swift in Sources */,
 				0CFCA3022891312F00A1A6B5 /* CreatePublisherViewModel.swift in Sources */,
 				21CF8E0528F875E8005E7CE6 /* SettingsViewModel.swift in Sources */,

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Helper/LocationSourceOption.swift
@@ -1,0 +1,28 @@
+import Foundation
+
+enum LocationSourceOption: Codable {
+    case phone
+    case s3File
+}
+
+extension LocationSourceOption {
+    func description() -> String {
+        switch self {
+        case .phone:
+            return "phone"
+        case .s3File:
+            return "S3 file"
+        }
+    }
+    
+    static func fromDescription(description: String) -> LocationSourceOption {
+        switch description {
+        case "phone":
+            return .phone
+        case "S3 file":
+            return .s3File
+        default:
+            fatalError()
+        }
+    }
+}

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Helper/S3Helper.swift
@@ -27,16 +27,25 @@ class S3Helper {
         case couldNotFindConfiguration
     }
     
+    private static var hasConfiguredAmplify = false
+    
     init() throws {
-            try Amplify.add(plugin: AWSCognitoAuthPlugin())
-            try Amplify.add(plugin: AWSS3StoragePlugin())
-            guard let url = Bundle.main.url(forResource: "amplifyconfiguration",
-                                            withExtension: "json",
-                                            subdirectory: "Optional Resources") else {
-                throw Error.couldNotFindConfiguration
-            }
-            let config = try AmplifyConfiguration(configurationFile: url)
-            try Amplify.configure(config)
+        guard !Self.hasConfiguredAmplify else {
+            return
+        }
+        
+        // Trying to configure Amplify more than once gives an error
+        Self.hasConfiguredAmplify = true
+        
+        try Amplify.add(plugin: AWSCognitoAuthPlugin())
+        try Amplify.add(plugin: AWSS3StoragePlugin())
+        guard let url = Bundle.main.url(forResource: "amplifyconfiguration",
+                                        withExtension: "json",
+                                        subdirectory: "Optional Resources") else {
+            throw Error.couldNotFindConfiguration
+        }
+        let config = try AmplifyConfiguration(configurationFile: url)
+        try Amplify.configure(config)
     }
     
     func fetchLocationHistoryFilenames() async throws -> [File] {

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Model/SettingsModel.swift
@@ -12,6 +12,7 @@ class SettingsModel {
     
     private let fallbackVehicleProfile = VehicleProfile.car
     private let fallbackRoutingProfile = RoutingProfile.driving
+    private let fallbackLocationSource = LocationSourceOption.phone
     
     var areRawLocationsEnabled: Bool {
         get {
@@ -91,6 +92,33 @@ class SettingsModel {
         }
         set {
             UserDefaults.standard.save(newValue.rawValue, forKey: "routingProfile")
+        }
+    }
+    
+    var locationSourceOption: LocationSourceOption {
+        get {
+            guard let value: LocationSourceOption = UserDefaults.standard.get("locationSource") else {
+                return fallbackLocationSource
+            }
+            
+            return value
+        }
+        set {
+            UserDefaults.standard.save(newValue, forKey: "locationSource")
+        }
+    }
+    
+    var s3FileName: String? {
+        get {
+            return UserDefaults.standard.get("s3FileName")
+        }
+        set {
+            if let newValue = newValue {
+                UserDefaults.standard.save(newValue, forKey: "s3FileName")
+            }
+            else {
+                UserDefaults.standard.removeObject(forKey: "s3FileName")
+            }
         }
     }
     

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/Model/SimulatedLocations.swift
@@ -1,120 +1,0 @@
-import CoreLocation
-
-struct SimulatedLocations {
-    
-    private static let data = [
-        "51.08653099232604,17.05007661290475",
-        "51.08649603705665,17.049990846643",
-        "51.0864604022401,17.04990191394837",
-        "51.08642402729132,17.0498057283104",
-        "51.08636805046392,17.04964382960347",
-        "51.08633232042072,17.04952566580025",
-        "51.08629823635223,17.04940820519394",
-        "51.08626610917858,17.04929181720329",
-        "51.08623675440912,17.0491724225888",
-        "51.08620855703261,17.04904959238377",
-        "51.08618275217101,17.04892756832133",
-        "51.08615845801366,17.04880606614219",
-        "51.08613654399829,17.04869351858959",
-        "51.08611792342654,17.04858737460739",
-        "51.08609920842829,17.04848214572321",
-        "51.08608189960203,17.04837993377062",
-        "51.08607494394776,17.04827702364209",
-        "51.0860697769149,17.04818364576298",
-        "51.08605749322106,17.04812289676909",
-        "51.08599854468796,17.04780817064384",
-        "51.08598301426149,17.04769583545695",
-        "51.08596518687549,17.04756999601158",
-        "51.08595225723369,17.04749147327787",
-        "51.08594301338269,17.04743793271971",
-        "51.08593288517179,17.04738022856477",
-        "51.08592561602343,17.04723511359736",
-        "51.08592793890762,17.04713029733593",
-        "51.08600385526821,17.04703993010119",
-        "51.08606688593952,17.04699010418303",
-        "51.08613751075229,17.04695469693618",
-        "51.08620642197278,17.04692619844568",
-        "51.08627551439734,17.04689503894101",
-        "51.08634368174013,17.04686368078774",
-        "51.08641520095393,17.0468261318734",
-        "51.08649320653463,17.04677892526972",
-        "51.08657274183366,17.046727223382",
-        "51.08665616890821,17.04667399092776",
-        "51.08674044622436,17.04662151268665",
-        "51.0868235949922,17.04657187382131",
-        "51.08690548293384,17.04652448366295",
-        "51.0869871930777,17.04647844040349",
-        "51.08707050921867,17.04643255617902",
-        "51.08715511103904,17.04638634987828",
-        "51.0872378103446,17.04634116642185",
-        "51.08731511455727,17.04629903813278",
-        "51.08738635716618,17.04625960364791",
-        "51.08745187124481,17.04622140432242",
-        "51.08751199984451,17.04618626610415",
-        "51.08757015800278,17.04615260216612",
-        "51.08762680398104,17.04612032467645",
-        "51.08768546521028,17.04608863233165",
-        "51.08774790399035,17.0460553228491",
-        "51.08781391411755,17.04602010037923",
-        "51.08788257922466,17.04598339333596",
-        "51.08795020374824,17.04594700265031",
-        "51.08801721910292,17.04591041928729",
-        "51.08808548120635,17.04587240106998",
-        "51.08815753260612,17.04583157869964",
-        "51.08823024892822,17.0457900492895",
-        "51.08830517669507,17.04574691866666",
-        "51.08837600182057,17.04570489040766",
-        "51.08846668549515,17.04565510933432",
-        "51.08852162722228,17.04562472968292",
-        "51.08856891153156,17.04559761931366",
-        "51.08861136225952,17.04557447076373",
-        "51.0886508921141,17.04555260617277",
-        "51.08868934169615,17.04552997410312",
-        "51.08872958418277,17.04550721653925",
-        "51.08877140226373,17.04548383643261",
-        "51.08881134454547,17.04546121911646",
-        "51.08884510854323,17.04544033644774",
-        "51.08887053457043,17.04543047574672",
-        "51.08902396399766,17.04536906305519",
-        "51.08904578206665,17.04535650127242",
-        "51.08906740533688,17.04533541562255",
-        "51.08921824592675,17.04521563503227",
-        "51.0892438676533,17.04520238494466",
-        "51.08927518159967,17.04518429525392",
-        "51.08937892044086,17.04512213165768",
-        "51.08943648408079,17.04509170073995",
-        "51.0894804260939,17.04506962832398",
-        "51.0895310279371,17.04504291602472",
-        "51.08958695129307,17.04501330062438",
-        "51.08964932885873,17.04497967748211",
-        "51.08971576918148,17.04494256077637",
-        "51.08978493394677,17.04490544770021",
-        "51.08985689900123,17.0448667786961",
-        "51.08992813197261,17.04482883961473",
-        "51.08999659383986,17.04479312396946",
-        "51.09005759199213,17.04475968656575",
-        "51.0901046586651,17.04473361197274",
-        "51.09012088764103,17.04469028972274",
-        "51.09016195594132,17.04443888885362",
-        "51.09015035548664,17.04433307302275",
-        "51.09014340215449,17.04426771995539"
-    ]
-    
-    static func recordedLocations() -> [CLLocation] {
-        return Self.data.enumerated().map { index, value -> CLLocation in
-            let values = value.split(separator: ",")
-            
-            return CLLocation(
-                coordinate: .init(latitude: Double(values.first!)!, longitude: Double(values.last!)!),
-                altitude: 100,
-                horizontalAccuracy: index < 10 ? 25 : 35,
-                verticalAccuracy: .zero,
-                course: .zero,
-                courseAccuracy: .zero,
-                speed: Double(8 + arc4random_uniform(7)), // speed between 8km/h to 15km/h
-                speedAccuracy: .zero,
-                timestamp: Date(timeIntervalSinceNow: Double(index))
-            )
-        }
-    }
-}

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/View/CreatePublisherView.swift
@@ -8,8 +8,13 @@ struct CreatePublisherView: View {
     @State var showConstantAccuracies = false
     @State var showVehicleProfiles = false
     @State var showRoutingProfiles = false
+    @State var showLocationSources = false
+    @State var showS3Files = false
     @State private var isShowingPublisherDetailsView = false
     @State private var publisher: ObservablePublisher?
+    @State private var isCreatingPublisher = false
+    @State private var error: ErrorInformation?
+    @State private var showAlert = false
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
@@ -64,6 +69,32 @@ struct CreatePublisherView: View {
                 }
                 
                 Section {
+                    TitleValueListItem(title: "Location Source", value: viewModel.locationSource.description())
+                        .onTapGesture {
+                            self.showLocationSources = true
+                        }
+                        .actionSheet(isPresented: $showLocationSources) {
+                            var buttons: [Alert.Button] = viewModel.locationSources.map { source in
+                                Alert.Button.default(Text(source)) {
+                                    viewModel.locationSource = LocationSourceOption.fromDescription(description: source)
+                                }
+                            }
+                            buttons.append(.cancel())
+                            return ActionSheet(
+                                title: Text("Location Source"),
+                                message: Text("Select location source"),
+                                buttons: buttons
+                            )
+                        }
+                    if viewModel.locationSource == .s3File {
+                        TitleValueListItem(title: "S3 File", value: viewModel.s3FileName ?? "")
+                            .onTapGesture {
+                                self.showS3Files = true
+                            }
+                            .sheet(isPresented: $showS3Files) {
+                                S3FilesView(fileName: $viewModel.s3FileName)
+                            }
+                    }
                     TitleValueListItem(title: "Vehicle Profile", value: viewModel.vehicleProfile.description())
                         .onTapGesture {
                             self.showVehicleProfiles = true
@@ -81,7 +112,7 @@ struct CreatePublisherView: View {
                                 buttons: buttons
                             )
                         }
-                    TitleValueListItem(title: "Routing profile", value: viewModel.routingProfile.description())
+                    TitleValueListItem(title: "Routing Profile", value: viewModel.routingProfile.description())
                         .onTapGesture {
                             self.showRoutingProfiles = true
                         }
@@ -93,8 +124,8 @@ struct CreatePublisherView: View {
                             }
                             buttons.append(.cancel())
                             return ActionSheet(
-                                title: Text("Vehicle Profile"),
-                                message: Text("Select vehicle profile"),
+                                title: Text("Routing Profile"),
+                                message: Text("Select routing profile"),
                                 buttons: buttons
                             )
                         }
@@ -104,15 +135,34 @@ struct CreatePublisherView: View {
                 
                 Section {
                     Button {
+                        isCreatingPublisher = true
                         // This is a temporary workaround so that the keyboard is no longer present if we
                         // navigate back to this screen. We can remove it after the fix to #421 below.
                         UIApplication.endEditing(true)
                         
                         viewModel.save()
-                        publisher = viewModel.createPublisher()
-                        isShowingPublisherDetailsView = true
+                        Task {
+                            do {
+                                publisher = try await viewModel.createPublisher()
+                                isShowingPublisherDetailsView = true
+                            }
+                            catch {
+                                self.error = .init(error: error)
+                                showAlert = true
+                            }
+                            isCreatingPublisher = false
+                        }
                     } label: {
-                        Text("Create publisher")
+                        HStack {
+                            Text("Create publisher")
+                                .padding(.trailing)
+                            ProgressView()
+                                .opacity(isCreatingPublisher ? 1 : 0)
+                        }
+                    }
+                    .disabled(isCreatingPublisher)
+                    .alert(isPresented: $showAlert) {
+                        .init(title: "Failed to create publisher", errorInformation: error)
                     }
                 }
                 

commit 7dcfc9c449d9fdcba386912527632c5f75336985

Publisher example: Make sure we start publisher from main thread
I noticed that in the publisher example app, the trackables are no
longer transitioning to the online state. This appears to have been
broken by a change in ac20806 where we started creating the publisher
inside an async function and hence potentially no longer on the main
thread.

This SDK does not explain the rules re which from which threads it can
be interacted with. I have created #445 to decide and document these
rules.

Examples/PublisherExampleSwiftUI/PublisherExampleSwiftUI/ViewModel/CreatePublisherViewModel.swift
@@ -96,7 +96,7 @@ class CreatePublisherViewModel: ObservableObject {
         }
     }
     
-    func createPublisher() async throws -> ObservablePublisher {
+    @MainActor func createPublisher() async throws -> ObservablePublisher {
         let connectionConfiguration = ConnectionConfiguration(apiKey: EnvironmentHelper.ABLY_API_KEY, clientId: "Asset Tracking Publisher Example")
         let resolution = Resolution(accuracy: .balanced, desiredInterval: 5000, minimumDisplacement: 100)
 