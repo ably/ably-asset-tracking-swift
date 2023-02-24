496c70c239921a561cb95bacc0ac8aa5fc785ec1
Andy Ford <andy.ford@ably.com>
2023-02-24 13:23:32 +0000

    Add test for PublisherWorkerFactory

---
+++ Tests/PublisherTests/WorkerQueue/PublisherWorkerFactoryTests.swift
@@ -0,0 +1,34 @@
+import XCTest
+import AblyAssetTrackingInternal
+import AblyAssetTrackingInternalTesting
+import AblyAssetTrackingPublisher
+import AblyAssetTrackingPublisherTesting
+
+class PublisherWorkerFactoryTests: XCTestCase
+{
+    private let logHandler = InternalLogHandlerMock.configured
+    private var legacyWorkerCalled = false
+    private let properties = PublisherWorkerQueueProperties()
+    private let factory = PublisherWorkerFactory()
+
+    func test_ItBuildsLegacyWork()
+    {
+        let callback = {[weak self] in
+            self?.legacyWorkerCalled = true
+            return
+        }
+
+        let worker = factory.createWorker(
+            workerSpecification: PublisherWorkSpecification.legacy(callback: callback),
+            logHandler: logHandler
+        )
+
+        XCTAssertTrue(worker is LegacyWorker<PublisherWorkerQueueProperties, PublisherWorkSpecification>)
+        let _ = try! worker.doWork(
+            properties: properties,
+            doAsyncWork: {_ in },
+            postWork: {_ in }
+        )
+        XCTAssertTrue(self.legacyWorkerCalled, "Legacy worker work not called")
+    }
+}
